8b63fbc3a3f29d932a1caf4ae5ef08b7eff117e1
Belén Albeza <belen@hey.com>
2023-07-03 17:32:00 +0200

    do not create canvas, but reuse existing one

--- src/lib.rs
+++ src/lib.rs
@@ -4,22 +4,12 @@ mod wasm;
 
 use wasm_bindgen::prelude::*;
 
-#[wasm_bindgen]
-pub fn run(container_id: &str) -> Result<(), JsValue> {
-    let cpu = cpu::CPU::new();
-    let mut vm = vm::VM::new(container_id, cpu);
-
-    vm.run().unwrap();
-
-    Ok(())
-}
-
 #[wasm_bindgen(js_name=loadRom)]
-pub fn load_rom(rom: js_sys::Uint8Array) -> Result<(), JsValue> {
-    let data = rom.to_vec();
-    for byte in data.into_iter() {
-        web_sys::console::log_1(&format!("{}", byte).into());
-    }
+pub fn load_rom(canvas_id: &str, rom: js_sys::Uint8Array) -> Result<(), JsValue> {
+    let cpu = cpu::CPU::new();
+    let mut vm = vm::VM::new(canvas_id, cpu);
+
+    vm.run(&rom.to_vec()).unwrap();
 
     Ok(())
 }
--- src/wasm.rs
+++ src/wasm.rs
@@ -4,6 +4,7 @@ use web_sys::ImageData;
 pub type Closure<T> = wasm_bindgen::closure::Closure<T>;
 pub type JsValue = wasm_bindgen::JsValue;
 pub type RenderContext = web_sys::CanvasRenderingContext2d;
+pub type CanvasElement = web_sys::HtmlCanvasElement;
 
 pub fn window() -> web_sys::Window {
     web_sys::window().expect("no global `window` exists")
@@ -13,27 +14,13 @@ pub fn document() -> web_sys::Document {
     window().document().expect("no document in window")
 }
 
-pub fn body() -> web_sys::HtmlElement {
-    document().body().expect("no body in document")
-}
-
-pub fn element_by_id(id: &str) -> Option<web_sys::HtmlElement> {
+pub fn element_by_id<T>(id: &str) -> Option<T>
+where
+    T: wasm_bindgen::JsCast,
+{
     document()
         .get_element_by_id(id)
-        .map(|x| x.dyn_into::<web_sys::HtmlElement>().unwrap())
-}
-
-pub fn create_canvas(width: u32, height: u32) -> web_sys::HtmlCanvasElement {
-    let canvas = document()
-        .create_element("canvas")
-        .expect("could not create <canvas>")
-        .dyn_into::<web_sys::HtmlCanvasElement>()
-        .unwrap();
-
-    canvas.set_width(width);
-    canvas.set_height(height);
-
-    canvas
+        .map(|x| x.dyn_into::<T>().unwrap())
 }
 
 pub fn canvas_context(canvas: &web_sys::HtmlCanvasElement) -> web_sys::CanvasRenderingContext2d {
@@ -54,4 +41,8 @@ pub fn request_animation_frame(f: &Closure<dyn FnMut()>) {
     window()
         .request_animation_frame(f.as_ref().unchecked_ref())
         .expect("should register `requestAnimationFrame` OK");
+}
+
+pub fn log(msg: &str) {
+    web_sys::console::log_1(&msg.into());
 }
