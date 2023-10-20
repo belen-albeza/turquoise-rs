474e3cc3e5e3fafc7e1cb2a347c00fe074593c1d
Belén Albeza <belen@hey.com>
2023-10-20 18:40:18 +0200

    renamed dist for docs for github pages

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 cargo build --release --target wasm32-unknown-unknown && \
-wasm-bindgen target/wasm32-unknown-unknown/release/turquoise_rs.wasm --out-dir dist --target web --no-typescript && \
-cp -r html/* dist/ && \
-mkdir -p dist/roms && cp roms/*.rom dist/roms/
+wasm-bindgen target/wasm32-unknown-unknown/release/turquoise_rs.wasm --out-dir docs --target web --no-typescript && \
+cp -r html/* docs/ && \
+mkdir -p docs/roms && cp roms/*.rom docs/roms/
